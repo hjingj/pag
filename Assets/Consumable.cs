95dd682682eb9f21bc6d20f33fbcef4c48d997d7
hjingj <hjingj@gmail.com>
2022-12-06 19:38:05 +0800

    creat pag project

---
+++ Assets/Consumable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ®ø¯Ó«~Ãþ, HP, MP
+/// </summary>
+public class Consumable : Item
+{
+    public int HP { get; set; }
+    public int MP { get; set; }
+
+    public Consumable(int id, string name, ItemType type, ItemQuality quality, string description,
+        int capacity, int buyPrice, int sellPrice, string sprite, int hp, int mp)
+        : base(id, name, type, quality, description, capacity, buyPrice, sellPrice, sprite)
+    {
+        this.HP = hp;
+        this.MP = mp;
+    }
+}
