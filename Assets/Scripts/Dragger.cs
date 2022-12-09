a356fa1e3be366dc8fc1bf3ab41417792df1cd62
hjingj <hjingj@gmail.com>
2022-12-09 20:06:14 +0800

    add dragger and dropper

---
+++ Assets/Scripts/Dragger.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Dragger : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public enum DraggerMode
+    {
+        CommonDrag = 0,
+        InstanceDrag = 1
+    }
+
+    private Image _image;
+    private Transform _originalParent;
+    public Canvas _canvas;
+    private bool _beInstanced;
+    private RectTransform _canvasRect;
+    private Vector3 _offsetCenter;
+
+    public DraggerMode DragMode = DraggerMode.CommonDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (DragMode == DraggerMode.CommonDrag)
+        {
+            Debug.Log("OnBeginDrag " + eventData.pointerDrag.name + ":" + gameObject.name);
+            _originalParent = _image.transform.parent;
+            _image.transform.SetParent(_canvas.transform);
+            _image.raycastTarget = false;
+            if (_canvas.renderMode == RenderMode.ScreenSpaceCamera)
+            {
+                Vector2 opos = Vector2.zero;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvasRect, Input.mousePosition, Camera.main, out opos);
+                Vector2 tpos = _image.transform.localPosition;
+                _offsetCenter = opos - tpos;
+            }
+            else if (_canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+            {
+                _offsetCenter = Input.mousePosition - _image.transform.position;
+            }
+        }
+        else
+        {
+            GameObject newImage = Instantiate(_image.gameObject);
+
+            Dragger d = newImage.GetComponent<Dragger>();
+            d.setup(_canvas, true);
+            newImage.transform.SetParent(_canvas.transform);
+            newImage.transform.localRotation = Quaternion.identity;
+            newImage.transform.localScale = Vector3.one;
+            newImage.GetComponent<Image>().raycastTarget = false;
+            eventData.pointerDrag = newImage;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 spos = Input.mousePosition;
+
+        if (_canvas.renderMode == RenderMode.ScreenSpaceCamera)
+        {
+            Vector2 opos = Vector2.zero;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvasRect, spos, Camera.main, out opos);
+            Vector2 tpos = _offsetCenter;
+            transform.localPosition = opos - tpos;
+        }
+        else if (_canvas.renderMode == RenderMode.ScreenSpaceOverlay)
+        {
+            _image.transform.position = spos - _offsetCenter;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (_image.transform.parent == _canvas.transform)
+        {
+            if (_beInstanced)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            _image.transform.SetParent(_originalParent);
+            _image.transform.localPosition = Vector3.zero;
+        }
+
+        if (_beInstanced)
+        {
+            _beInstanced = false;
+        }
+
+        Debug.Log("OnEndDrag " + eventData.pointerDrag.name + ":" + gameObject.name);
+        _image.raycastTarget = true;
+    }
+
+    public Transform getOriginalParent()
+    {
+        return _originalParent;
+    }
+
+    public void setup(Canvas cs, bool bInstance)
+    {
+        _canvas = cs;
+        _image = GetComponent<Image>();
+        _canvasRect = _canvas.GetComponent<RectTransform>();
+        _beInstanced = bInstance;
+    }
+
+    public bool isInstanced()
+    {
+        return _beInstanced;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image = GetComponent<Image>();
+        _canvasRect = _canvas.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dropper : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        int inChild = transform.childCount;
+        Debug.Log("OnDrop " + gameObject.name + " : " + eventData.pointerDrag.name);
+        GameObject dragObject = eventData.pointerDrag;
+        if (inChild > 0)
+        {
+            Dragger d = dragObject.GetComponent<Dragger>();
+            if (d.isInstanced())
+            {
+                Transform c = transform.GetChild(0);
+                Destroy(c.gameObject);
+            }
+            else
+            {
+                Transform p = d.getOriginalParent();
+                Transform c = transform.GetChild(0);
+                c.SetParent(p);
+                c.localPosition = Vector3.zero;
+            }
+        }
+        Transform tDrag = dragObject.transform;
+        tDrag.SetParent(transform);
+        tDrag.localPosition = Vector3.zero;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
