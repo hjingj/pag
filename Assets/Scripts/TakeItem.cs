22e64f67bdaea101379a0e1b122275de2bdd3135
hjingj <hjingj@gmail.com>
2022-12-14 15:07:38 +0800

    test: TakeItem test.

--- Assets/Scripts/TakeItem.cs
+++ Assets/Scripts/TakeItem.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class TakeItem : MonoBehaviour
 {
+    private Image[,] slot = new Image[10, 6];
     public Image _image;
 
     // Update is called once per frame
@@ -12,7 +13,23 @@ public class TakeItem : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Instantiate(_image, _image.transform);
+            AutoPutItem();
+        }
+    }
+
+    void AutoPutItem()
+    {
+        foreach (Item item in slot)
+        {
+            if (slot != null)
+                {
+                check next slot;
+            }
+        else if (slot == null)
+            {
+                Instantiate(_image, slot.transform);
+                return;
+            }
         }
     }
 }
